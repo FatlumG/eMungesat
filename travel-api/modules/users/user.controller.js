55375fd351c462d28e1261183d97c4f049a952c4
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-04-19 15:28:38 +0200

    Add profile avatar

--- travel-api/modules/users/user.controller.js
+++ travel-api/modules/users/user.controller.js
@@ -139,3 +139,16 @@ export const deleteMe = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error });
   }
 };
+
+export const getMe = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error });
+  }
+};
